751f0e75b9af88f32e44737caf3f5601270bac26
Dario <dariojim01@gmail.com>
2024-04-30 21:29:28 -0500

    CRUD Books

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res)=>{
     return res.status(234).send("WELCOME MERN DJ");
 });
 
-//Route
+//Route para crear un book
 app.post('/books', async (req, res)=>{
     try{
         if(
@@ -41,6 +41,77 @@ app.post('/books', async (req, res)=>{
 
     }
 
+});
+//Route para obtener los books
+app.get('/books', async (req, res)=>{
+    try {
+        const books = await Book.find({});
+
+        return res.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error) {
+        res.status(500).send({message: error.message});
+    }
+});
+//Route para obtener un book por ID
+app.get('/books/:id', async (req, res)=>{
+    try {
+        const { id } = req.params;
+        const books = await Book.findById(id);
+
+        return res.status(200).json(books);
+    } catch (error) {
+        res.status(500).send({message: error.message});
+    }
+});
+//Route para actualizar un book por ID
+app.put('/books/:id', async (req, res)=>{
+    try {
+        if(
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+        ){
+            return res.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+
+        const { id } = req.params;
+        const books = await Book.findByIdAndUpdate(id, req.body);
+        if(!books){
+            return res.status(404).send({
+                message: 'Book not found',
+            });
+        }
+        return res.status(200).send({
+            message: 'Book actualizado',
+        });
+    } catch (error) {
+        res.status(500).send({message: error.message});
+    }
+});
+
+//Route para borrar un book por ID
+app.delete('/books/:id', async (req, res)=>{
+    try {
+       
+
+        const { id } = req.params;
+        const books = await Book.findByIdAndDelete(id);
+        if(!books){
+            return res.status(404).send({
+                message: 'Book not found',
+            });
+        }
+        return res.status(200).send({
+            message: 'Book eliminado',
+        });
+    } catch (error) {
+        res.status(500).send({message: error.message});
+    }
 });
 
 mongoose
