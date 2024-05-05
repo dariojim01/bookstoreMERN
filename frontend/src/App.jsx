1d085c24cd35c94e69a3bf9db5a73ff375e2c5d0
Dario <dariojim01@gmail.com>
2024-05-04 21:07:53 -0500

    Initializing frontend with vite and tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='' element={} />
+      <Route path='' element={} />
+      <Route path='' element={} />
+      <Route path='' element={} />
+      <Route path='' element={} />
+    </Routes>
+  )
+}
+
+export default App
